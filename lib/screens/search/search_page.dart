85437ae136d5630483c78f5b469aac0c599ed3c8
ahmed ragab <ar1755@fayoum.edu.eg>
2023-11-05 13:52:49 +0200

    refactor code

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -1,10 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:moviesapp/shared/style/colors/app_colores.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(23.0),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: AppColors.moviesBackGround,
+            ),
+            child: TextField(
+              style: Theme.of(context).textTheme.bodyMedium,
+              cursorColor: AppColors.selectedIconColor,
+              decoration: InputDecoration(
+                hintText: 'Search',
+                hintStyle: Theme.of(context)
+                    .textTheme
+                    .bodyMedium!
+                    .copyWith(fontSize: 17),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  color: AppColors.selectedIconColor,
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide:
+                      const BorderSide(color: AppColors.selectedIconColor),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide:
+                      const BorderSide(color: AppColors.selectedIconColor),
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  borderSide:
+                      const BorderSide(color: AppColors.selectedIconColor),
+                ),
+              ),
+            ),
+          ),
+          const Spacer(),
+          Image.asset(
+            'assets/images/no_movie.png',
+            width: 100,
+            height: 115,
+            fit: BoxFit.fill,
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,20 +2,17 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:moviesapp/layout/home_page.dart';
 
-
-
-
 class SplashScreen extends StatefulWidget {
   static const String routeName = 'splash';
 
-  const SplashScreen({super.key});
+  const SplashScreen({Key? key}) : super(key: key); 
+
   @override
-  _SplashScreen2SubState createState() => _SplashScreen2SubState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreen2SubState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> {
   bool _a = false;
-
   @override
   void initState() {
     super.initState();
@@ -25,8 +22,7 @@ class _SplashScreen2SubState extends State<SplashScreen> {
       });
     });
     Timer(const Duration(milliseconds: 4000), () {
-      Navigator.of(context)
-          .pushReplacement(SlideTransitionAnimation (const HomeScreen()));
+      Navigator.of(context).pushReplacement(SlideTransitionAnimation(const HomeScreen()));
     });
   }
 
@@ -47,11 +43,12 @@ class _SplashScreen2SubState extends State<SplashScreen> {
             color: Colors.black,
           ),
           Center(
-            child: Image.asset('assets/images/splash_screen.png',
-            width: 150,
-            height: 150,
+            child: Image.asset(
+              'assets/images/splash_screen.png',
+              width: 150,
+              height: 150,
               fit: BoxFit.fill,
-            )
+            ),
           ),
         ],
       ),
@@ -64,18 +61,19 @@ class SlideTransitionAnimation extends PageRouteBuilder {
 
   SlideTransitionAnimation(this.page)
       : super(
-      pageBuilder: (context, animation, anotherAnimation) => page,
-      transitionDuration: const Duration(milliseconds: 2000),
-      transitionsBuilder: (context, animation, anotherAnimation, child) {
-        animation = CurvedAnimation(
-          curve: Curves.fastLinearToSlowEaseIn,
-          parent: animation,
+          pageBuilder: (context, animation, anotherAnimation) => page,
+          transitionDuration: const Duration(milliseconds: 2000),
+          transitionsBuilder: (context, animation, anotherAnimation, child) {
+            animation = CurvedAnimation(
+              curve: Curves.fastLinearToSlowEaseIn,
+              parent: animation,
+            );
+            return SlideTransition(
+              position: Tween(begin: const Offset(1.0, 0.0), end: const Offset(0.0, 0.0))
+                  .animate(animation),
+              textDirection: TextDirection.rtl,
+              child: child,
+            );
+          },
         );
-        return SlideTransition(
-          position: Tween(begin: const Offset(1.0, 0.0), end: const Offset(0.0, 0.0))
-              .animate(animation),
-          textDirection: TextDirection.rtl,
-          child: page,
-        );
-      });
 }
